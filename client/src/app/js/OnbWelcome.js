f931756762b61a4e95e39cf8f623112135023ce3
Greg Jackson <greg@talendro.com>
2025-11-14 13:26:50 -0500

    Add 'See How It Works' CTA button to welcome page bottom section

--- client/src/app/js/OnbWelcome.js
+++ client/src/app/js/OnbWelcome.js
@@ -69,6 +69,7 @@ export default function Page(){
       
       <div className='mt-8 flex flex-wrap gap-3 justify-center'>
         <a href='/app/onboarding/step-1'><button className='btn btn-primary mr-3 text-lg py-3 px-6'>Upload Resume</button></a>
+        <a href='/how-it-works'><button className='btn btn-secondary mr-3 text-lg py-3 px-6'>See How It Works</button></a>
       </div>
     </section>
   )
