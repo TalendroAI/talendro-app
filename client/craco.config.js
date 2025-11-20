832b092fadd27e434a0de6ab6918dacc7cedee45
Greg Jackson <greg@talendro.com>
2025-11-20 18:00:03 -0500

    Update CRACO config to support Tailwind CSS processing

--- client/craco.config.js
+++ client/craco.config.js
@@ -2,6 +2,14 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
     allowedHosts: 'all'
-  }
+  },
+  style: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
 };
 
